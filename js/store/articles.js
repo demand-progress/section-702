f4f139d817ece55d919b7167c16cea3713c6da1a
Jeff Dangerfield <jeff@Zekes-MBP.T-mobile.com>
2017-10-12 12:50:30 -0700

    Changed 'title' to 'origin' in query params

--- js/store/articles.js
+++ js/store/articles.js
@@ -3,3 +3,14 @@ export const articles = [
     {body: "Lorem Ipsum and then Some! (2"},
     {body: "Lorem Ipsum. What does it mean? (3"},
 ]
+
+export const origin = {
+    afterAction: {
+        title: 'Thank you for signing',
+        subTitle:'Now could you call your member of congress? \n It\'s the most effective way to have your voice heard.'
+    },
+    default: {
+        title: 'Tell Congress:',
+        subTitle:'Stop illegal wiretapping'
+    },
+}
